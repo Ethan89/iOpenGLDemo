4cf18a3c43ae0f92bfcd809908f571779421d727
Ethan <yaofeng.guo@gmail.com>
2017-02-14 15:33:45 +0800

    add OpenGLView

---
+++ iOpenGLDemo/OpenGLView.h
@@ -0,0 +1,13 @@
+//
+//  OpenGLView.h
+//  iOpenGLDemo
+//
+//  Created by Ethan Guo on 17/2/14.
+//  Copyright © 2017年 com.bjdv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OpenGLView : UIView
+
+@end
---
+++ iOpenGLDemo/OpenGLView.m
@@ -0,0 +1,21 @@
+//
+//  OpenGLView.m
+//  iOpenGLDemo
+//
+//  Created by Ethan Guo on 17/2/14.
+//  Copyright © 2017年 com.bjdv. All rights reserved.
+//
+
+#import "OpenGLView.h"
+
+@implementation OpenGLView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
