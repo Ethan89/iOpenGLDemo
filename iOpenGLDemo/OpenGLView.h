56bb787e46499d0ea0280546d28fe144c0229ec2
Ethan <yaofeng.guo@gmail.com>
2017-02-14 15:35:00 +0800

    fix OpenGLView

--- iOpenGLDemo/OpenGLView.h
+++ iOpenGLDemo/OpenGLView.h
@@ -7,7 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+#import <OpenGLES/ES2/gl.h>
+#import <OpenGLES/ES2/glext.h>
 
 @interface OpenGLView : UIView
+
+@property (strong, nonatomic) CAEAGLLayer *eaglLayer;
+@property (strong, nonatomic) EAGLContext *context;
+@property (nonatomic) GLuint colorRenderBuffer;
+@property (nonatomic) GLuint frameBuffer;
 
 @end
--- iOpenGLDemo/ViewController.m
+++ iOpenGLDemo/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "OpenGLView.h"
 
 @interface ViewController ()
+
+@property (strong, nonatomic) OpenGLView *openGLView;
 
 @end
 
@@ -17,13 +20,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.openGLView setFrame:[[UIScreen mainScreen] bounds]];
+    [self.view addSubview:self.openGLView];
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (OpenGLView *)openGLView {
+    if (!_openGLView) {
+        _openGLView = [OpenGLView new];
+    }
+    return _openGLView;
+}
 
 @end
