e867b9acb1867c9e8e33825b5378b96faf32a30f
Ethan <yaofeng.guo@gmail.com>
2017-02-14 10:07:13 +0800

    project init

---
+++ iOpenGLDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOpenGLDemo
+//
+//  Created by Ethan Guo on 17/2/14.
+//  Copyright © 2017年 com.bjdv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOpenGLDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOpenGLDemo
+//
+//  Created by Ethan Guo on 17/2/14.
+//  Copyright © 2017年 com.bjdv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
